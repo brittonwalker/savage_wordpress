4c3f2b69b40e8ead1010aec8b234ea17f6e44f11
Britton Walker <britton@forofficeuseonly.com>
2017-12-03 20:28:17 -0500

    Show unlimited posts

--- public/wp-content/themes/savage/index.php
+++ public/wp-content/themes/savage/index.php
@@ -8,24 +8,28 @@ $context = Timber::get_context();
 
 $args = array(
     'post_type' => 'painting',
+    'posts_per_page' => -1,
 );
 
 $context['paintings'] = Timber::get_posts( $args );
 
 $args = array(
     'post_type' => 'drawing',
+    'posts_per_page' => -1,
 );
 
 $context['drawings'] = Timber::get_posts( $args );
 
 $args = array(
     'post_type' => 'album_art',
+    'posts_per_page' => -1,
 );
 
 $context['album_art'] = Timber::get_posts( $args );
 
 $args = array(
     'post_type' => 'other',
+    'posts_per_page' => -1,
 );
 
 $context['other'] = Timber::get_posts( $args );
